67c0821181a81f58012d8c35f69548efaad390b7
Henrik Stanley Mortensen <hemor@dfds.com>
2020-08-07 12:09:20 +0200

    change to use postgress based on environment variables

--- packages/backend/knexfile.ts
+++ packages/backend/knexfile.ts
@@ -15,10 +15,24 @@
  */
 
 module.exports = {
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3',
+  //   },
+  // },
+
   development: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './dev.sqlite3',
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
     },
   },
 
